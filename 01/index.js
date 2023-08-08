4fa2977426575f653e2088694a4c3015b0c17372
Tiago Kennedy <128265741+T-St0rm@users.noreply.github.com>
2023-08-08 18:58:31 -0400

    respondido

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco ={
+    rua: "josafim buarques",
+    numero: 261,
+    complemento: "casa",
+    bairro: "petropolis",
+    cep: 69063550,
+    cidade: "Manaus",
+    estado: "Amazonas",
+    pais: "Breasil"
+}
+
+const usuario ={
+    nome: "Tiago Kennedy",
+    email: "tiago@gmail.com",
+    telefone: "(92) 99279-4226",
+    data_nascimento: "06/12/2002",
+    endereco: endereco
+}
+
+console.log(usuario)
